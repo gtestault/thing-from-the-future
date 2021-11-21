902a5ea224afcb6b129049fe282edc072330f17d
silkeunrau <silke.unrau@yahoo.de>
2021-11-21 19:33:26 +0100

    add playfield.component, card.component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { PlayfieldComponent } from './playfield/playfield.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    PlayfieldComponent
   ],
   imports: [
     BrowserModule,
