fa2980e9e5918c4efaeb73cc698c57f2e9174418
silkeunrau <silke.unrau@yahoo.de>
2021-11-29 17:23:31 +0100

    style cards

--- frontend/src/app/card-field/card-field.component.ts
+++ frontend/src/app/card-field/card-field.component.ts
@@ -33,6 +33,54 @@ export class CardFieldComponent implements OnInit {
       term2: "",
       time: ""
     },
+    {
+      category: "object",
+      term: "postcard",
+      term2: "",
+      time: ""
+    },
+    {
+      category: "mood",
+      term: "melancholy",
+      term2: "",
+      time: ""
+    },
+    {
+      category: "terrain",
+      term: "grandma's house",
+      term2: "water",
+      time: ""
+    },
+    {
+      category:  "arc",
+      term: "grow",
+      term2: "",
+      time: ""
+    },
+    {
+      category: "object",
+      term: "postcard",
+      term2: "",
+      time: ""
+    },
+    {
+      category: "mood",
+      term: "melancholy",
+      term2: "",
+      time: ""
+    },
+    {
+      category: "terrain",
+      term: "grandma's house",
+      term2: "water",
+      time: ""
+    },
+    {
+      category:  "arc",
+      term: "grow",
+      term2: "",
+      time: ""
+    },
   ];
 
 
