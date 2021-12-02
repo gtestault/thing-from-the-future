89cf064eced6f3729ceccb07b6fff8a94800b67b
silkeunrau <silke.unrau@yahoo.de>
2021-12-02 21:14:16 +0100

    add new components

---
+++ frontend/src/app/card-field/card-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-field',
+  templateUrl: './card-field.component.html',
+  styleUrls: ['./card-field.component.scss']
+})
+export class CardFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/empty-card-space/empty-card-space.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-card-space',
+  templateUrl: './empty-card-space.component.html',
+  styleUrls: ['./empty-card-space.component.scss']
+})
+export class EmptyCardSpaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
