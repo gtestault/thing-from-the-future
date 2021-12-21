8218f38b10070625a2b77481ab50f85d56a2f0da
gtestault <gabriel.testault@logmein.com>
2021-12-21 22:33:52 +0100

    add player module with mongodb storage

---
+++ backend/src/player/schemas/player.schema.ts
@@ -0,0 +1,13 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+
+export type PlayerDocument = Player & Document
+
+@Schema()
+export class Player {
+   @Prop()
+   _id: string
+   @Prop()
+   username: string
+}
+
+export const PlayerSchema = SchemaFactory.createForClass(Player);
