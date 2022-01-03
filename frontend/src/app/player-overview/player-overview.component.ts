9c006d86b5e6be1555ada05293d12dba07e277ed
silkeunrau <silke.unrau@yahoo.de>
2022-01-03 18:35:45 +0100

    style player overview, display players

--- frontend/src/app/player-overview/player-overview.component.ts
+++ frontend/src/app/player-overview/player-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../../models/Player';
 
 @Component({
   selector: 'app-player-overview',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerOverviewComponent implements OnInit {
 
+  players: Player[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    //TODO: get players
+    this.players = [
+      {
+        name: 'player1',
+        id: 1
+      },
+      {
+        name: 'susi123',
+        id: 2
+      },
+      {
+        name: 'hello_x',
+        id: 3
+      },
+      {
+        name: 'lena92',
+        id: 4
+      }
+
+        ]
+
   }
 
 }
