192a48143fbdec984e5739c78d8cd6b4f5a30c19
silkeunrau <silke.unrau@yahoo.de>
2022-01-03 18:56:00 +0100

    get players in player-overview.component from player.service

--- frontend/src/app/player-overview/player-overview.component.ts
+++ frontend/src/app/player-overview/player-overview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from '../../models/Player';
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-player-overview',
@@ -10,29 +11,11 @@ export class PlayerOverviewComponent implements OnInit {
 
   players: Player[] = [];
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit(): void {
     //TODO: get players
-    this.players = [
-      {
-        name: 'player1',
-        id: 1
-      },
-      {
-        name: 'susi123',
-        id: 2
-      },
-      {
-        name: 'hello_x',
-        id: 3
-      },
-      {
-        name: 'lena92',
-        id: 4
-      }
-
-        ]
+    this.players = this.playerService.getPlayers();
 
   }
 
--- frontend/src/services/player.service.ts
+++ frontend/src/services/player.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Player} from '../models/Player';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
+  private players: Player[] = [
+    {
+      name: 'player1',
+      id: 1
+    },
+    {
+      name: 'susi123',
+      id: 2
+    },
+    {
+      name: 'hello_x',
+      id: 3
+    },
+    {
+      name: 'lena92',
+      id: 4
+    }
+
+  ]
+
   constructor() { }
+
+  //TODO: Set player array, remove dummy data
+
+  getPlayers(): Player[] {
+    return this.players;
+  }
 }
