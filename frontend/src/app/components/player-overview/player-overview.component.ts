751494795943e544f2e5977b133122457fac7562
silkeunrau <silke.unrau@yahoo.de>
2022-01-06 15:59:49 +0100

    adjust imports

--- frontend/src/app/components/player-overview/player-overview.component.ts
+++ frontend/src/app/components/player-overview/player-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Player } from '../../models/Player';
-import { PlayerService } from '../services/player.service';
+import { Player } from '../../../models/Player';
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-player-overview',
--- frontend/src/app/components/turn-indicator/turn-indicator.component.ts
+++ frontend/src/app/components/turn-indicator/turn-indicator.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {PlayerService} from '../services/player.service';
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-turn-indicator',
