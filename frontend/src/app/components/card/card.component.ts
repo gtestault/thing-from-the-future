86d2318372325d484a538771139e6d6d4fb779ac
silkeunrau <silke.unrau@yahoo.de>
2022-03-11 15:41:45 +0100

    start implement set card

--- frontend/src/app/components/card/card.component.ts
+++ frontend/src/app/components/card/card.component.ts
@@ -13,10 +13,34 @@ export class CardComponent implements OnInit {
   @Input() term = "";
   @Input() time = ""
 
-  turnAround = false;
+  dragPosition = {x: 0, y: 0};
+  positionAbsolute = false;
 
   ngOnInit(): void {
 
   }
 
+  changePosition(category: string, position: any) {
+    // if ( // noch nicht gelegt ) {
+    //
+    this.positionAbsolute = !this.positionAbsolute;
+      if (category === 'arc') {
+        this.dragPosition = {x: 50, y: 50};
+      } else if (category === 'terrain') {
+
+      } else if (category === 'object') {
+        // berechne position
+      } else if (category === 'mood') {
+        // berechne position
+      } else {
+      }
+    //
+    // }
+
+
+    // this.dragPosition = {x: this.dragPosition.x + 50, y: this.dragPosition.y + 50};
+    console.log(category);
+    console.log(position);
+    console.log(this.dragPosition);
+  }
 }
