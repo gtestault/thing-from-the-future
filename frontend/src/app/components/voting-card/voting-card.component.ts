6a34a10d8d38d20e9acdad5ebc02f96e0998c160
silkeunrau <silke.unrau@yahoo.de>
2022-03-15 01:39:37 +0100

    style voting page

--- frontend/src/app/components/voting-card/voting-card.component.ts
+++ frontend/src/app/components/voting-card/voting-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-voting-card',
@@ -7,11 +7,20 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class VotingCardComponent implements OnInit {
 
+  @HostBinding('style.voted')
+  border = '2px solid $tftf-green';
+
   @Input() text = '';
+
+  voted = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  vote() {
+    this.voted = true;
   }
 
 }
